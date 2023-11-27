7fcda047ba1fd89385217746a7d2ed1077512912
GatlinThompson <112907390+GatlinThompson@users.noreply.github.com>
2023-11-26 22:59:58 -0700

    added use effect to reset page when rendering

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,12 @@ import Jumbotron from "../components/home/Jumbotron";
 import PhoneApp from "../components/home/PhoneApp";
 import Specials from "../components/home/Specials";
 import FancyButton from "../elements/FancyButton";
+import { useEffect } from "react";
 
 const HomePage = () => {
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "instant" });
+  }, []);
   return (
     <main>
       <section className=" px-0 px-lg-5 container-fluid">
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,7 +1,11 @@
 import Header from "../elements/Header";
 import Menu from "../components/menu/Menu";
+import { useEffect } from "react";
 
 const MenuPage = () => {
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "instant" });
+  }, []);
   return (
     <main>
       <Header title={"Menu"} addClass={"page-header"} />
