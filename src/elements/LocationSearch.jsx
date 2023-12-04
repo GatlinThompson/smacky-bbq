c6cebeffc2e4767bdc2c236bd970eeb0d996764a
GatlinThompson <112907390+GatlinThompson@users.noreply.github.com>
2023-12-03 21:15:09 -0700

    test 4

--- src/elements/LocationSearch.jsx
+++ src/elements/LocationSearch.jsx
@@ -6,38 +6,30 @@ const LocationSearch = (props) => {
   const [locations, setLocations] = useState([]);
   const [seeLocations, setSeeLocations] = useState(false);
 
-  const handleSearch = async (value) => {
-    try {
-      const response = await axios.get(
-        `/.netlify/functions/locations?input=${encodeURIComponent(value)}`
-      );
-      setLocations(response.data);
-      console.log(response.data);
-    } catch (error) {
-      console.error("Error fetching predictions:", error);
-    }
-    setSeeLocations(true);
-  };
-
   const changeHandler = (event) => {
     const value = event.target.value;
     setQuery(value);
     props.setLocation(event.target.value);
-    handleSearch(value);
 
     // Call the Google Places Autocomplete API
-    //   axios
-    //     .get(
-    //       `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${value}&key=AIzaSyDvnTtdpTq402kGwLhB3ivwChkLWB8v6yM`
-    //     )
-    //     .then((response) => {
-    //       setLocations(response.data.predictions.slice(0, 4));
-    //     })
-    //     .catch((error) => {
-    //       console.error("Error fetching predictions:", error);
-    //     });
+    axios
+      .get(
+        `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${value}&key=AIzaSyDvnTtdpTq402kGwLhB3ivwChkLWB8v6yM`,
+        {
+          headers: {
+            Authorization: "Bearer AIzaSyDvnTtdpTq402kGwLhB3ivwChkLWB8v6yM",
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((response) => {
+        setLocations(response.data.predictions.slice(0, 4));
+      })
+      .catch((error) => {
+        console.error("Error fetching predictions:", error);
+      });
 
-    //   setSeeLocations(true);
+    setSeeLocations(true);
   };
 
   const changeLocation = (event) => {
@@ -77,7 +69,18 @@ const LocationSearch = (props) => {
       </div>
       {seeLocations && locations.length >= 1 && (
         <div className="location-results mx-auto">
-          <ul></ul>
+          <ul>
+            {locations.map((location) => (
+              <li
+                key={location.place_id}
+                onClick={changeLocation}
+                onMouseDown={changeLocation}
+              >
+                <i className="bi bi-geo-alt-fill location-icon"></i>
+                {location.description}
+              </li>
+            ))}
+          </ul>
         </div>
       )}
     </>
